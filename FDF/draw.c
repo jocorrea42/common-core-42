9849175f03d65347ecba40110978fbf0b17dc1ca
jocorrea42 <jrcorrearodriguez@gmail.com>
2025-10-08 00:22:29 +0200

    fdf

---
+++ FDF/draw.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jocorrea <jocorrea@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/29 15:03:04 by jocorrea          #+#    #+#             */
+/*   Updated: 2023/07/30 15:14:09 by jocorrea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+void	isometric(float *x, float *y, float angle, int z)
+{
+	*x = (*x - *y) * cos(angle);
+	*y = (*x + *y) * sin(angle) - z;
+}
+
+void	put_pixel(t_point *p, t_fdf *data)
+{
+	int		max;
+	float	x_step;
+	float	y_step;
+
+	x_step = (p->x1 - p->x);
+	y_step = p->y1 - p->y;
+	max = max1(mod1(x_step), mod1(y_step));
+	x_step /= max;
+	y_step /= max;
+	while ((int)(p->x - p->x1) || (int)(p->y - p->y1))
+	{
+		mlx_pixel_put(data->mlx_ptr, data->win_ptr, p->x, p->y, data->color);
+		p->x += x_step;
+		p->y += y_step;
+	}
+}
+
+int	bresenham(t_point *p, t_fdf *data)
+{
+	int		z;
+	int		z1;
+
+	z = data->map[(int)(p->y)][(int)(p->x)] * data->factor;
+	z1 = data->map[(int)(p->y1)][(int)(p->x1)] * data->factor;
+	p->x *= data->zoom;
+	p->y *= data->zoom;
+	p->x1 *= data->zoom;
+	p->y1 *= data->zoom;
+	data->color = 0xffffff;
+	if (z1 > 0 || z > 0)
+		data->color = 0xe80c0c;
+	else if (z < 0 || z1 < 0)
+		data->color = 0x0000FF;
+	isometric(&(p->x), &(p->y), data->angle, z);
+	isometric(&(p->x1), &(p->y1), data->angle, z1);
+	p->x += data->shift_x;
+	p->y += data->shift_y;
+	p->x1 += data->shift_x;
+	p->y1 += data->shift_y;
+	put_pixel(p, data);
+	return (1);
+}
+
+void	set_p(int x, int y, t_point *p, t_fdf *data)
+{
+	if (x < data->width - 1)
+	{
+		p->x = x;
+		p->y = y;
+		p->x1 = x + 1;
+		p->y1 = y; 
+		bresenham(p, data);
+	}
+	if (y < data->height -1)
+	{
+		p->x = x;
+		p->y = y;
+		p->x1 = x;
+		p->y1 = y + 1; 
+		bresenham(p, data);
+	}
+}
+
+int	draw(t_fdf *data)
+{
+	int		x;
+	int		y;
+	t_point	*p;
+
+	p = (t_point *)malloc(sizeof(t_point));
+	if (!p)
+		return (ft_err("can't make the point"));
+	y = 0;
+	while (y < data->height)
+	{
+		x = 0;
+		while (x < data->width)
+		{
+			set_p(x, y, p, data);
+			x++;
+		}
+		y++;
+	}
+	free(p);
+	return (1);
+}
---
+++ FDF/read_file.c
@@ -0,0 +1,118 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   read_file.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jocorrea <jocorrea@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/29 12:29:24 by jocorrea          #+#    #+#             */
+/*   Updated: 2023/07/30 14:25:51 by jocorrea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+static int	ft_count_word(char const *s, char c)
+{
+	int	i;
+	int	word;
+
+	i = 0;
+	word = 0;
+	while (s && s[i])
+	{
+		if (s[i] != c)
+		{
+			word++;
+			while ((s[i] != c) && s[i])
+				i++;
+		}
+		else
+			i++;
+	}
+	return (word);
+}
+
+int	fill_data(char *file, t_fdf *data)
+{
+	char	*line;
+	int		fd;
+
+	fd = open(file, O_RDONLY, 0);
+	if (fd < 0)
+		return (ft_err("can't open file"));
+	line = get_next_line(fd);
+	data->width = ft_count_word(line, ' ');
+	data->height = 0;
+	while (line)
+	{
+		data->height++;
+		free(line);
+		line = get_next_line(fd);
+	}
+	free(line);
+	close(fd);
+	data->map = (int **)malloc(sizeof(int *) * (data->height + 1));
+	if (!data->map)
+		return (ft_err("can't fill row"));
+	data->map[data->height] = NULL;
+	return (0);
+}
+
+void	fill_map(int *map, char *line)
+{
+	char	**nums;
+	int		i;
+
+	nums = ft_split(line, ' ');
+	i = 0;
+	while (nums[i])
+	{
+		map[i] = ft_atoi(nums[i]);
+		free(nums[i]);
+		i++;
+	}
+	free(nums);
+}
+
+int	fill_attr_data(int fd, t_fdf *data)
+{
+	char	*line;
+	int		i;
+
+	data->zoom = 20;
+	data->shift_x = 350;
+	data->shift_y = 350;
+	data->angle = 0.56;
+	data->factor = 1;
+	i = 0;
+	line = get_next_line(fd);
+	while (data->map[i])
+	{
+		data->map[i] = (int *)malloc(sizeof(int) * (data->width));
+		if (!(data->map[i]))
+		{
+			map_free(i, data);
+			return (ft_err("can't malloc"));
+		}
+		fill_map(data->map[i++], line);
+		free(line);
+		line = get_next_line(fd);
+	}
+	free(line);
+	return (1);
+}
+
+int	read_file(char *file, t_fdf *data)
+{
+	int		fd;
+
+	if (fill_data(file, data) < 0)
+		return (-1);
+	fd = open(file, O_RDONLY, 0);
+	if (fd < 0)
+		return (ft_err("can't open file"));
+	fill_attr_data(fd, data);
+	close(fd);
+	return (1);
+}
